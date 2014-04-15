9650a8798d06bbb69efc798ac15e21a74ba49695
Ujjwal Ojha <bring2uo@gmail.com>
2014-04-15 20:32:29 +0545

    Added SQL schema

---
+++ data/schema.sql
@@ -0,0 +1,9 @@
+﻿delimiter $$
+
+CREATE TABLE `settings` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(64) NOT NULL,
+  `namespace` varchar(24) NOT NULL,
+  `value` varchar(256) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8$$
