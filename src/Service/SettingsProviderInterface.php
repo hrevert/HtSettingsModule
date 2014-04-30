e8d25167b83e0d86c4c2e30700a0987922b28d17
Ujjwal Ojha <bring2uo@gmail.com>
2014-04-30 16:23:05 +0545

    Added ability to get array settings

--- src/Service/SettingsProviderInterface.php
+++ src/Service/SettingsProviderInterface.php
@@ -4,6 +4,14 @@ namespace HtSettingsModule\Service;
 interface SettingsProviderInterface
 {
     /**
+     * Gets settings in array format
+     *
+     * @param  string $namespace
+     * @return array
+     */
+    public function getSettingsArray($namespace);    
+
+    /**
      * Gets settings of a namespace
      *
      * @param  string $namespace
